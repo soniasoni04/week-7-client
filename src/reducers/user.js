a5d4774db1fd67ce1eddd6f69c0698ba83531e7c
Asanga <asanga.nicholas@yahoo.com>
2019-10-28 18:10:38 +0100

    create signup and login forms

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,5 +1,11 @@
+import {USER_LOGIN} from '../action'
+
+
 export default function (state = [], action = {}) {
     switch (action.type) {
+      case USER_LOGIN:
+        return action.payload
+        
       default:
         return state
     }
