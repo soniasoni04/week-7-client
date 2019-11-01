1e11568ab49ee9daba9ce3c8761e742864a5e12a
Asanga <asanga.nicholas@yahoo.com>
2019-11-01 10:22:45 +0100

    some few changes

--- src/components/Lobby.js
+++ src/components/Lobby.js
@@ -2,20 +2,22 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import LobbyForm from './LobbyForm';
+
 import Game from './Game';
-import Player from './Player';
-import LobbyId from './LobbyId'
+// import Player from './Player';
+// import LobbyId from './LobbyId'
 // import './lobby.css';
 
 class Lobby extends React.Component {
 	render () {
-		const { lobbies, jwt } = this.props;
+		const { lobbies,  } = this.props;
 		console.log('lobbies test:', lobbies)
 		const lobbyList = lobbies.map(game => <Game game={game} key={game.id} />)
 
 		return (
 			<div>
 			<LobbyForm />
+	
 				
 				<div className="gameList">{lobbyList}</div>
 				<button onClick={() => this.props.history.push('/')}>Go Back</button>
